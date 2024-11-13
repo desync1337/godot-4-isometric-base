c213916d82f5fffc2873622a21ba70e603fd3419
desync1337 <151230370+desync1337@users.noreply.github.com>
2024-11-13 17:49:46 +0300

    Add files via upload

--- godot_tycoonbase/scripts/cameracontroller.gd
+++ godot_tycoonbase/scripts/cameracontroller.gd
@@ -8,9 +8,9 @@ extends Camera2D
 	#[2,0] sand
 
 var mode = "view" # [build , view]
-var selectbuild = "warehouse"
-
+@onready var qmenu = get_parent().get_node("qmenu")
 @onready var cursor = get_parent().get_node("3dcursor")
+@onready var world = get_parent()
 
 @onready var warehouse = preload("res://scenes/warehouse.tscn")
 @onready var coalfac = preload("res://scenes/coalfactory.tscn")
@@ -20,7 +20,13 @@ func _input(event: InputEvent) -> void:
 	$debugtext.text = str(
 		"celltype: " , G.celltype, 
 		"\ncellglobalpos: ", G.globalpos,
-		"\nmode: ", mode)
+		"\nmode: ", mode,
+		"\nselectedbuild: ", G.selectbuild,
+		"\nmoney: ", G.money,
+		"\ncoal: ",G.coal,
+		"\ngematit: ",G.gematit,
+		"\niron: " ,G.iron,
+		"\nlead: ")
 	
 	if Input.is_action_just_pressed("+scroll") and zoom.x < 4.3:
 		zoom.x += 0.5
@@ -36,16 +42,23 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("+x"):
 		mode = "delete"
 	
+	if Input.is_action_just_pressed("+q"):
+		qmenu.visible = true
+		qmenu.position = Vector2(G.clickpos.x, G.clickpos.y + 117)
+		
+	
 	G.clickpos = get_global_mouse_position()
-	G.cellpos = get_parent().local_to_map(Vector2(G.clickpos.x,G.clickpos.y))
-	G.celltype = get_parent().get_cell_atlas_coords(G.cellpos)
-	G.globalpos = get_parent().map_to_local(G.cellpos)
+	G.cellpos = world.local_to_map(Vector2(G.clickpos.x,G.clickpos.y))
+	G.celltype = world.get_cell_atlas_coords(G.cellpos)
+	G.globalpos = world.map_to_local(G.cellpos)
 
 	if mode == "view":
 		cursor.visible = false
 		if event is InputEventScreenDrag:
 			position -= (event.relative * 0.2) * zoom.x
-			
+		
+		if Input.is_action_just_pressed("+g"):
+			world.generate_island()
 	
 	if mode == "build" && G.celltype.x > -1:
 		cursor.visible = true
@@ -54,7 +67,7 @@ func _input(event: InputEvent) -> void:
 		
 		if event is InputEventScreenTouch:
 			if event.is_pressed():
-				trybuild(G.globalpos * 0.1, selectbuild)
+				trybuild(G.cellpos, G.selectbuild)
 	
 	if mode == "delete" && G.celltype.x > -1:
 		cursor.visible = true
@@ -64,25 +77,26 @@ func _input(event: InputEvent) -> void:
 		if event is InputEventScreenTouch:
 			if event.is_pressed():
 				trydelete(G.globalpos)
+				print(G.globalpos)
 				
 
 func trydelete(pos):
-	for e in get_parent().get_child_count(): #looking for object on cell
-		var object = get_parent().get_child(e)
+	for e in world.get_child_count(): #looking for object on cell
+		var object = world.get_child(e)
 		
 		if object.position == pos && object.name != "3dcursor":
 
 			object.queue_free()
-			G.nobuild.erase(pos * 0.1)
+			world.nobuild.erase(pos)
 			break
 			
 func trybuild(pos,type) -> void:
-	var clickpos = get_parent().local_to_map(get_global_mouse_position())
+	var clickpos = world.local_to_map(get_global_mouse_position())
 	var id = G.buildlist.find(type)
 	#check allowed materials
-	if get_parent().get_cell_atlas_coords(clickpos) == G.buildlist[id + 1]:
-		if !G.nobuild.has(pos): #check if occupied
-			build(pos,type)
+	if world.get_cell_atlas_coords(clickpos) == G.buildlist[id + 1]:
+		if !world.nobuild.has(world.map_to_local(pos)): #check if occupied
+			build(G.globalpos,type)
 		else:
 			print_rich("[color=red]Cell is occupied!!![/color]")
 	else:
@@ -92,18 +106,19 @@ func build(pos,type):
 	match type:
 		"coal":
 			var instcoalfac = coalfac.instantiate()
-			instcoalfac.position = pos * 10
-			get_parent().add_child(instcoalfac)
-			G.nobuild.append(pos)
+			instcoalfac.position = pos
+			world.add_child(instcoalfac)
+			world.nobuild.append(pos)
 		"warehouse":
 			var instwarehouse = warehouse.instantiate()
-			instwarehouse.position = pos * 10
-			get_parent().add_child(instwarehouse)
-			G.nobuild.append(pos)
+			instwarehouse.position = pos
+			world.add_child(instwarehouse)
+			world.nobuild.append(pos)
+	G.selectbuild = "none"
 	print_rich("[color=green]cell avaible! building...[/color]")
 	
 
 func _physics_process(delta: float) -> void:
-	get_parent().get_node("water").position.x = position.x - 1111
-	get_parent().get_node("water").position.y = position.y - 1111
+	world.get_node("water").position.x = position.x - 1111
+	world.get_node("water").position.y = position.y - 1111
 	
--- godot_tycoonbase/scripts/generated.gd
+++ godot_tycoonbase/scripts/generated.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@export var price = 50
+
 #append yourself to nobuild otherwise we're gonna have problems!
 func _ready() -> void:
-	G.nobuild.append(position * 0.1)
+	if get_parent().nobuild.has(position):
+		queue_free()
+	else:
+		get_parent().nobuild.append(position)
